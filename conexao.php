aa0b4c7d1845f2a99601537b50f75930ecd3fea2
Matheus123456 <matheus_onlive@live.com>
2021-02-25 19:59:22 -0300

    Update

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
 
-$conexao = mysqli_connect("grupocontem.com.br", "grupocon_conexao", "c0Nt3m#2@1p", "grupocon_conexaocontem_bd") or die("Sem conexao");
+$conexao = mysqli_connect("", "", "", "") or die("Sem conexao");
 
 ?>
